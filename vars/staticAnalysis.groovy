d42d069ec808dfcff50a236fa4a5261c70fabe33
Cecilia Dominguez <ceciidom98@gmail.com>
2024-11-14 12:21:46 -0600

    sonar server fixed

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,11 +1,8 @@
-// Archivo: vars/staticAnalysis.groovy
 def call(Map config = [:]) {  // Acepta un mapa de configuración opcional
     stage('Static Code Analysis') {
-        withSonarQubeEnv('SonarQube') {
+        withSonarQubeEnv('sq1') {
             bat 'echo "Ejecución de las pruebas de calidad de código"'
-            
-            // Aquí podrías agregar el comando real para ejecutar SonarQube o análisis estático
-        }
+                    }
         
         // Verificar el parámetro abortPipeline si está configurado en true
         if (config.get('abortPipeline', false)) {

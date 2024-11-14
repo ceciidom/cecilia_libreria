f35420ff4815f6b3aa3c8016ba07d9d5a529aff0
Cecilia Dominguez <ceciidom98@gmail.com>
2024-11-14 11:56:38 -0600

    Groovy without quality gate

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,23 +1,9 @@
-def call(boolean abortPipeline = false, int timeout = 5) {
-    // Iniciar análisis estático de SonarQube
+// Archivo: vars/staticAnalysis.groovy
+def call() {
     stage('Static Code Analysis') {
-        withSonarQubeEnv('SonarQube') { // Ajusta el nombre del servidor SonarQube si es necesario
-            bat 'echo "Ejecución de las pruebas de calidad de código"' // Aquí reemplazamos sh con bat
-        }
-    }
-    
-    // Esperar el resultado del quality gate de SonarQube
-    stage('Quality Gate') {
-        timeout(time: timeout, unit: 'MINUTES') {
-            def qualityGate = waitForQualityGate()
-            if (qualityGate.status != 'OK') {
-                echo "Quality Gate failed: ${qualityGate.status}"
-                if (abortPipeline) {
-                    error "Pipeline abortado debido a fallo en el Quality Gate."
-                }
-            } else {
-                echo "Quality Gate passed."
-            }
+        withSonarQubeEnv('SonarQube') { // Ajusta 'SonarQube' al ID de tu servidor en Jenkins
+            // Usa bat para Windows y sh para Linux
+            bat 'echo "Ejecución de las pruebas de calidad de código"' // o usa sh si estás en Linux
         }
     }
 }

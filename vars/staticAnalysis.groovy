053c2afd8ea4c264ed90354e8af81ed1ae527290
Cecilia Dominguez <ceciidom98@gmail.com>
2024-11-14 12:45:57 -0600

    simulated result added to quality gate mock

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,18 +1,19 @@
-def call(Map config = [:]) { 
+// Archivo: vars/sonarAnalysis.groovy
+def call(Map config = [:]) {
     boolean abortOnQualityGate = config.get('abortOnQualityGate', false)
     boolean abortPipeline = config.get('abortPipeline', false)
-    boolean mockQualityGate = config.get('mockQualityGate', false)
-    
+    boolean mockQualityGate = config.get('mockQualityGate', false)  // Simulación de QualityGate
+    String simulatedResult = config.get('simulatedResult', 'OK')   // Resultado simulado del QualityGate
+
     stage('Static Code Analysis') {
         withSonarQubeEnv('sq1') {
             bat 'echo "Ejecución de las pruebas de calidad de código"'
         }
-        
+
+        // Espera de QualityGate con simulación de resultado
         timeout(time: 5, unit: 'MINUTES') {
-            if(mockQualityGate) {
+            if (mockQualityGate) {
                 echo "Simulando resultado del QualityGate..."
-                def simulatedResult = "OK"
-                
                 if (simulatedResult != "OK" && abortOnQualityGate) {
                     echo "QualityGate fallido: abortando el pipeline"
                     currentBuild.result = 'ABORTED'
@@ -20,23 +21,21 @@ def call(Map config = [:]) {
                 } else {
                     echo "QualityGate aprobado: continuando con el pipeline"
                 }
-
             } else {
                 echo "Esperando resultado del QualityGate..."
-                def qg = waitForQualityGate()  
-
+                def qg = waitForQualityGate()
                 if (qg.status != 'OK' && abortOnQualityGate) {
                     echo "QualityGate fallido: abortando el pipeline"
                     currentBuild.result = 'ABORTED'
                     error("Pipeline abortado debido al fallo en el QualityGate")
                 }
             }
-        } 
+        }
 
         if (abortPipeline) {
-                echo "Abortando el pipeline debido a la configuración de abortPipeline"
-                currentBuild.result = 'ABORTED'
-                error("Pipeline abortado debido a la configuración de abortPipeline")
-            }    
+            echo "Abortando el pipeline debido a la configuración de abortPipeline"
+            currentBuild.result = 'ABORTED'
+            error("Pipeline abortado debido a la configuración de abortPipeline")
+        }
     }
 }
